8f754d823dd2f578e8636463ff5b1c3fde030bdb
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-03-30 09:12:02 +0200

    Update playlist.py

--- YukkiMusic/utils/inline/playlist.py
+++ YukkiMusic/utils/inline/playlist.py
@@ -28,18 +28,15 @@ def botplaylist_markup(_):
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
             ),
-        ], 
-    ]
-    return buttons
-
-
-def botplaylist_markup(_):
-    buttons = [
+        ],
         [
             InlineKeyboardButton(
-                text=_["PL_B_12"], url=f"https://t.me/so_alfaa"
+                text=_["PL_B_12"], url=f"https://t.me/so_alfaa" ),
             ),
-        ], 
+            InlineKeyboardButton(
+                text=_["PL_B_13"], url=f"https://t.me/S_D_H_A" ),
+            ),
+        ],
     ]
     return buttons
 
