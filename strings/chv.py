257fa854fd04ec0cfbefe4d9f14923b44c02d766
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-03-31 22:31:29 +0200

    Update chv.py

--- strings/chv.py
+++ strings/chv.py
@@ -1,5 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
+from staring.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
