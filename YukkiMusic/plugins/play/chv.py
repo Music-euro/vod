69ea6383306a3acb91ad2292a2c50efc6ed8b552
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-03-31 22:53:26 +0200

    Update chv.py

--- YukkiMusic/plugins/play/chv.py
+++ YukkiMusic/plugins/play/chv.py
@@ -1,10 +1,15 @@
 import asyncio
 from pyrogram import Client, filters
+from strings import get_command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 
-@app.on_message(filters.command(["اسلام"]) & filters.group & ~filters.edited)
+@app.on_message(
+    filters.command(["اسلام"])
+    & filters.group
+    & ~filters.edited
+)
 async def khalid(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph//file/e0bbcfb36e9c9f196e828.jpg",
