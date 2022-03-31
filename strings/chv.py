a5f5e4679754235a5c7a68b414c52270c9ea3f7d
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-03-31 22:32:49 +0200

    Update chv.py

--- strings/chv.py
+++ strings/chv.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 
-@app.on_message(filters.command(["اسلام"]) & filters.group & ~filters.edited)
+@app.on_message(command(["اسلام"]) & filters.group & ~filters.edited)
 async def khalid(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph//file/e0bbcfb36e9c9f196e828.jpg",
