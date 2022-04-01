e8fc3f4d287d0acd5a98bc22c317e887445366f6
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-01 17:45:24 +0200

    Update بوت.py

--- YukkiMusic/plugins/play/بوت.py
+++ YukkiMusic/plugins/play/بوت.py
@@ -29,7 +29,7 @@ async def khalid(client: Client, message: Message):
                 ],[
                     InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
                 ],
             ]
         ),
