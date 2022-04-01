30c516be726a4a8773599db33c468b6fdf34674a
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-01 14:03:20 +0200

    Update مطور بوت.py

--- YukkiMusic/plugins/play/مطور بوت.py
+++ YukkiMusic/plugins/play/مطور بوت.py
@@ -1,4 +1,26 @@
 import asyncio
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
+from youtubesearchpython.__future__ import VideosSearch
+
+import config
+from config import BANNED_USERS
+from config.config import OWNER_ID
+from strings import get_command, get_string
+from YukkiMusic import Telegram, YouTube, app
+from YukkiMusic.misc import SUDOERS
+from YukkiMusic.plugins.play.playlist import del_plist_msg
+from YukkiMusic.plugins.sudo.sudoers import sudoers_list
+from YukkiMusic.utils.database import (add_served_chat,
+                                       add_served_user,
+                                       blacklisted_chats,
+                                       get_assistant, get_lang,
+                                       get_userss, is_on_off,
+                                       is_served_private_chat)
+from YukkiMusic.utils.decorators.language import LanguageStart
+from YukkiMusic.utils.inline import (help_pannel, private_panel,
+                                     start_pannel)
 from pyrogram import Client, filters
 from strings import get_command
 from config.config import OWNER_ID
