3dc95be36d8def64524038019b2b23d5a8bbe9ad
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-01 17:36:42 +0200

    Create بوت.py

---
+++ YukkiMusic/plugins/play/بوت.py
@@ -0,0 +1,29 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+    command(["بوت"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph//file/ad292b6f6fbc05c824919.jpg",
+        caption=f"""مبرمج سورس لورا""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝘿𝙀𝙑 ¦ 𝙀𝙎𝙇𝘼𝙈", url=f"https://t.me/S_D_H_A"),
+                ],[
+                    InlineKeyboardButton(
+                        "اضفني الي محجموعتك", url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                ],
+            ]
+        ),
+    )
