3e34edd47713bce1dd1435a93b1c34d6be483d57
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-02 00:57:27 +0200

    Update ccc.py

--- YukkiMusic/plugins/play/ccc.py
+++ YukkiMusic/plugins/play/ccc.py
@@ -48,7 +48,7 @@ async def ft(_, query: CallbackQuery):
                     InlineKeyboardButton(
                         "اغلاق", callback_data="close"),
                     InlineKeyboardButton(
-                        "رجوع", callback_data=f"ft"),
+                        "رجوع", callback_data=f"fft"),
                ],
           ]
         ),
