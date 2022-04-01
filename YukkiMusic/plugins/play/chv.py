eb7df3fa1a80662f45e615eb071c73a8cef09adf
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-01 11:39:16 +0200

    Update chv.py

--- YukkiMusic/plugins/play/chv.py
+++ YukkiMusic/plugins/play/chv.py
@@ -22,7 +22,7 @@ async def khalid(client: Client, message: Message):
                         "𝘿𝙀𝙑 ¦ 𝙀𝙎𝙇𝘼𝙈", url=f"https://t.me/S_D_H_A"),
                  [
                     InlineKeyboardButton(
-                        "مطورين السورس", callback_data=f"ftt"),
+                        "مطورين السورس", callback_data=f"fft"),
                 ],
             ]
         ),
