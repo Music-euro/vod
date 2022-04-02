4f038f3cfbfab1b2f60add9ef62ca3fbe268999f
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-02 23:17:04 +0200

    Update مطور بوت.py

--- YukkiMusic/plugins/play/مطور بوت.py
+++ YukkiMusic/plugins/play/مطور بوت.py
@@ -54,7 +54,7 @@ OWNER = getenv("OWNER")
 )
 async def khalid(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph//file/ad292b6f6fbc05c824919.jpg",
+        photo=f"https://telegra.ph/file/955def7fc34f0df8f6fa9.jpg",
         caption=f"""[مطوري مشغول ياقلبي💞🥺](https://t.me/so_alfaa)""",
         reply_markup=InlineKeyboardMarkup(
             [
