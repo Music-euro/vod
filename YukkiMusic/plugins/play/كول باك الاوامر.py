6d691160e02eb8885110c4e5d2e3f3c958ca85eb
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-04 01:49:26 +0200

    Update كول باك الاوامر.py

--- YukkiMusic/plugins/play/كول باك الاوامر.py
+++ YukkiMusic/plugins/play/كول باك الاوامر.py
@@ -53,3 +53,11 @@ async def tt(_, query: CallbackQuery):
           ]
         ),
     )
+
+
+@app.on_callback_query(filters.regex("back"))
+async def back(_, query: CallbackQuery):
+   await query.edit_message_text(
+       f"""لاوامر الاغاني اضغط ⇐ ⓵\n\nلاوامر البوت اضغط ⇐ ②""",
+       reply_markup=InlineKeyboardMarkup(
+ [ [ InlineKeyboardButton( "⓵", callback_data=f"tt"), InlineKeyboardButton( "②", callback_data=f"tt"), ],[ InlineKeyboardButton( "- sᴏᴜʀᴄᴇ ʟᴜʀᴀ .", callback_data=f"fft"), ], ] ), )
