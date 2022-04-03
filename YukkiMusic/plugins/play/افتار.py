3d1fc9c1b5d00d56632924174d0a85d3ec84e42c
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-03 16:18:25 +0200

    Create افتار.py

---
+++ YukkiMusic/plugins/play/افتار.py
@@ -0,0 +1,29 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+
+@app.on_message(
+    command(["افتار"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/25e38d524c71a62fae429.jpg",
+        caption=f"""- افتار مبرمج سورس لورا .""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "- ᴀᴠᴀᴛᴀʀ .", url=f"https://t.me/H_B_W"),
+                ],[
+                    InlineKeyboardButton(
+                        "- sᴏᴜʀᴄᴇ ʟᴜʀᴀ .", callback_data=f"fft"),
+                ],
+            ]
+        ),
+    )
