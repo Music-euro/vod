2a0f67a457cada152ca8ef7954660a5c71da8fba
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-04 16:26:09 +0200

    Update ماديسون رتبتي.py

--- YukkiMusic/plugins/play/ماديسون رتبتي.py
+++ YukkiMusic/plugins/play/ماديسون رتبتي.py
@@ -3,7 +3,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@app.on_message(command(["رتبتي"]) & filters.user(2059448162))
+@app.on_message(command(["رتبتي"])
+    & filters.user(2059448162))
 async def motawer(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/02ff5eed4dc2a34cbc1f7.jpg",
