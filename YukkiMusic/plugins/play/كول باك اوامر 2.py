444da40fe6b74246f1f0df7c86beb44f80408050
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-04 02:59:59 +0200

    Update كول باك اوامر 2.py

--- YukkiMusic/plugins/play/كول باك اوامر 2.py
+++ YukkiMusic/plugins/play/كول باك اوامر 2.py
@@ -55,8 +55,7 @@ async def ttt(_, query: CallbackQuery):
     )
 
 
-
- @app.on_callback_query(filters.regex("back1"))
+@app.on_callback_query(filters.regex("back1"))
 async def back1(_, query: CallbackQuery):
    await query.edit_message_text(
        f"""لاوامر الاغاني اضغط ⇐ ⓵\n\nلاوامر البوت اضغط ⇐ ②""",
