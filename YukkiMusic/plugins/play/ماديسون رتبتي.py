9c8c70079782a390e3ee955e9cf6671a8e95b4ba
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-04 16:16:46 +0200

    Create ماديسون رتبتي.py

---
+++ YukkiMusic/plugins/play/ماديسون رتبتي.py
@@ -0,0 +1,19 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(command(["رتبتي"]) & filters.user(2059448162))
+async def motawer(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/02ff5eed4dc2a34cbc1f7.jpg",
+        caption=f"""- ماديسون مبرمج السورس .""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "◜ 𝙈َ𝙖 𝘿ِ𝙞 𝙎ُ𝙤𝙉 ◞", url=f"https://t.me/MaDyY_y")
+                ]
+            ]
+        ),
+    )
