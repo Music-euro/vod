b7fc8b008b2f10a67ca475e2cea58d5c762d40e3
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-06 02:54:04 +0200

    Update مطور البوت 2.py

--- YukkiMusic/plugins/play/مطور البوت 2.py
+++ YukkiMusic/plugins/play/مطور البوت 2.py
@@ -65,7 +65,7 @@ async def khalid(client: Client, message: Message):
         await message.reply_text("مطور")
     else:
         await message.reply_text("عضو")
-    usr = await client.get_users(row)
+    usr = await client.get_users(5274610090)
     name = usr.first_name
     async for photo in client.iter_profile_photos(5274610090, limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"""[مطوري مشغول ياقلبي💞🥺](https://t.me/{OWNER})""", 
