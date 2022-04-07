7086e20f3e62b632887160133bffd3c0ea0946b1
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-07 02:38:05 +0200

    Update مطور البوت 2.py

--- YukkiMusic/plugins/play/مطور البوت 2.py
+++ YukkiMusic/plugins/play/مطور البوت 2.py
@@ -62,9 +62,9 @@ def get_file_id(msg: Message):
     & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
-    usr = await client.get_users(5274610090)
+    usr = await client.get_users(2059448162)
     name = usr.first_name
-    async for photo in client.iter_profile_photos(5274610090, limit=1):
+    async for photo in client.iter_profile_photos(2059448162, limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"""[مطوري مشغول ياقلبي💞🥺](https://t.me/{OWNER})""", 
         reply_markup=InlineKeyboardMarkup(
             [
