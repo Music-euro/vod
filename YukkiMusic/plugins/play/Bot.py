23f1ff211c2a30c25088bd6bbfa8eda83c076cba
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-09 17:13:15 +0200

    Update Bot.py

--- YukkiMusic/plugins/play/Bot.py
+++ YukkiMusic/plugins/play/Bot.py
@@ -1,81 +1,128 @@
 import asyncio
+
 from pyrogram import Client, filters
+
 from strings import get_command
+
 from strings.filters import command
+
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
 from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
 from typing import Union
+
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+
 from YukkiMusic import app
+
 from config import BANNED_USERS, MUSIC_BOT_NAME
+
 from YukkiMusic.misc import SUDOERS
 
 import re
+
 import sys
+
 from os import getenv
 
 from dotenv import load_dotenv
+
 from pyrogram import filters
 
 load_dotenv()
 
 BOT_USERNAME = getenv("BOT_USERNAME")
 
-IMG_DEV1 = getenv("IMG_DEV1")
-
 OWNER = getenv("OWNER")
 
-BOTID = getenv("BOTID11")
-
-
-
+ID_BOT1 = getenv("ID_BOT1")
 
 def get_file_id(msg: Message):
+
     if msg.media:
+
         for message_type in (
+
             "photo",
+
             "animation",
+
             "audio",
+
             "document",
+
             "video",
+
             "video_note",
+
             "voice",
+
             # "contact",
+
             # "dice",
+
             # "poll",
+
             # "location",
+
             # "venue",
+
             "sticker",
+
         ):
+
             obj = getattr(msg, message_type)
+
             if obj:
+
                 setattr(obj, "message_type", message_type)
+
                 return obj
 
-
-
 @app.on_message(
+
     command(["البوت"])
+
     & filters.group
+
     & ~filters.edited
+
 )
-async def alid(client: Client, message: Message):
-    usr = await client.get_users(BOTID11)
+
+async def khalid(client: Client, message: Message):
+
+    usr = await client.get_users(ID_BOT1)
+
     name = usr.first_name
-    async for photo in client.iter_profile_photos(BOTID11, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""[مطوري مشغول ياقلبي💞🥺](https://t.me/{OWNER})""", 
+
+    async for photo in client.iter_profile_photos(ID_BOT1, limit=1):
+
+                    await message.reply_photo(photo.file_id,       caption=f"اسمي [{MUSIC_BOT_NAME}](https://t.me/{BOT_USERNAME}) يقلبي 🙄💕", 
+
         reply_markup=InlineKeyboardMarkup(
+
             [
+
                 [
+
                     InlineKeyboardButton(
-                        name, url=f"https://t.me/{OWNER}")
+
+                        "- Devoleper Bot .", url=f"https://t.me/{OWNER}") 
+
                 ],[
+
                     InlineKeyboardButton(
+
                         "اضف البوت الي مجموعتك", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+
                 ],
+
             ]
+
         ),
+
     )
 
