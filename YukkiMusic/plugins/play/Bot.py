7468e4185ce6e6eef0d7086eb6a82e12e713f474
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-09 20:09:33 +0200

    Update Bot.py

--- YukkiMusic/plugins/play/Bot.py
+++ YukkiMusic/plugins/play/Bot.py
@@ -32,6 +32,27 @@ from dotenv import load_dotenv
 
 from pyrogram import filters
 
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from typing import Union
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from YukkiMusic import app
+from config import BANNED_USERS, MUSIC_BOT_NAME
+from YukkiMusic.misc import SUDOERS
+
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
 load_dotenv()
 
 BOT_USERNAME = getenv("BOT_USERNAME")
