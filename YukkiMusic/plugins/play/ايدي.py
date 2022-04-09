32c96407f43511a3b3b2f548bd2be4a7218e6d58
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-09 03:27:06 +0200

    Create ايدي.py

---
+++ YukkiMusic/plugins/play/ايدي.py
@@ -0,0 +1,74 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_command
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from typing import Union
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from YukkiMusic import app
+from config import BANNED_USERS, MUSIC_BOT_NAME
+from YukkiMusic.misc import SUDOERS
+
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+BOT_USERNAME = getenv("BOT_USERNAME")
+
+
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker",
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
+
+
+@app.on_message(
+    command(["ايدي"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(m.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(m.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""[عايز اي انتا 😒](https://t.me/{m.from_user.id})""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{m.from_user.id}")
+                ],[
+                    InlineKeyboardButton(
+                        "اضف البوت الي مجموعتك", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+                ],
+            ]
+        ),
+    )
