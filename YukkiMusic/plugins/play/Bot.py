c9f8e1f9037047e2f47dd9983a9614a2ab9eeb87
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-09 05:47:25 +0200

    Update Bot.py

--- YukkiMusic/plugins/play/Bot.py
+++ YukkiMusic/plugins/play/Bot.py
@@ -10,18 +10,6 @@ from pyrogram.types import InlineKeyboardButton
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
 from YukkiMusic import app
 from config import BANNED_USERS, MUSIC_BOT_NAME
-import asyncio
-from pyrogram import Client, filters
-from strings import get_command
-from strings.filters import command
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from typing import Union
-from pyrogram.types import InlineKeyboardButton
-
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from YukkiMusic import app
-from config import BANNED_USERS, MUSIC_BOT_NAME
 from YukkiMusic.misc import SUDOERS
 
 import re
@@ -30,20 +18,17 @@ from os import getenv
 
 from dotenv import load_dotenv
 from pyrogram import filters
-import re
-import sys
-from os import getenv
 
-from dotenv import load_dotenv
-from pyrogram import filters
 
 load_dotenv()
 
 BOT_USERNAME = getenv("BOT_USERNAME")
 
 IMG_BOT1 = getenv("IMG_BOT1")
+
 OWNER = getenv("OWNER")
-BOTID = getenv("BOTID1")
+
+BOTID = getenv("BOT_ID")
 
 
 def get_file_id(msg: Message):
@@ -76,9 +61,9 @@ def get_file_id(msg: Message):
     & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
-    usr = await client.get_users(BOTID1)
+    usr = await client.get_users(BOT_ID)
     name = usr.first_name
-    async for photo in client.iter_profile_photos(BOTID1, limit=1):
+    async for photo in client.iter_profile_photos(BOT_ID, limit=1):
                     await message.reply_photo(photo.file_id,       caption=f"اسمي [{MUSIC_BOT_NAME}](https://t.me/{BOT_USERNAME}) يقلبي 🙄💕", 
         reply_markup=InlineKeyboardMarkup(
             [
