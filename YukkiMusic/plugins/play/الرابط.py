14815ba69e7fdbf898a01a4917961749a5bce090
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-10 18:54:44 +0200

    Create الرابط.py

---
+++ YukkiMusic/plugins/play/الرابط.py
@@ -0,0 +1,123 @@
+import asyncio
+
+from pyrogram import Client, filters
+
+from strings import get_command
+
+from strings.filters import command
+
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+from config import LOG, LOG_GROUP_ID
+
+from YukkiMusic import app
+
+from YukkiMusic.utils.database import is_on_off
+
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+
+from YukkiMusic import app
+
+from config import BANNED_USERS, MUSIC_BOT_NAME
+
+from YukkiMusic.misc import SUDOERS
+
+import re
+
+import sys
+
+from os import getenv
+
+from dotenv import load_dotenv
+
+from pyrogram import filters
+
+load_dotenv()
+
+BOT_USERNAME = getenv("BOT_USERNAME")
+
+def get_file_id(msg: Message):
+
+    if msg.media:
+
+        for message_type in (
+
+            "photo",
+
+            "animation",
+
+            "audio",
+
+            "document",
+
+            "video",
+
+            "video_note",
+
+            "voice",
+
+            # "contact",
+
+            # "dice",
+
+            # "poll",
+
+            # "location",
+
+            # "venue",
+
+            "sticker",
+
+        ):
+
+            obj = getattr(msg, message_type)
+
+            if obj:
+
+                setattr(obj, "message_type", message_type)
+
+                return obj
+
+@app.on_message(
+
+    command(["الرابط"])
+
+    & filters.group
+
+    & ~filters.edited
+
+)
+
+async def khalid(client: Client, message: Message):
+
+    usr = await client.get_users(chatusername)
+
+    name = usr.first_name
+
+    async for photo in client.iter_profile_photos(chatusername, limit=1):
+
+                    await message.reply_photo(photo.file_id,       caption=f"""**Chat Link:** {chatusername}""", 
+
+        reply_markup=InlineKeyboardMarkup(
+
+            [
+
+                [
+
+                    InlineKeyboardButton(
+
+                        name, url=f"https://t.me/{chatusername}")
+
+                ],
+
+            ]
+
+        ),
+
+    )
