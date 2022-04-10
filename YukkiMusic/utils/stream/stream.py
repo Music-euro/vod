f1fbc7c65191642b293968cc0e1aa37ceb0a2154
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-10 14:54:38 +0200

    Add files via upload

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -37,7 +37,6 @@ async def stream(
     result,
     chat_id,
     user_name,
-    ghosttt,
     original_chat_id,
     video: Union[bool, str] = None,
     streamtype: Union[bool, str] = None,
@@ -114,11 +113,7 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                user = await app.get_users(user_id)
-                photo_id = user.photo.big_file_id if user.photo else None
-                photo = await app.download_media(photo_id)
-                img = await gen_thumb(vidid, photo)
-                title = result["title"]
+                img = await gen_thumb(vidid)
                 button = stream_markup(_, vidid)
                 await app.send_photo(
                     original_chat_id,
@@ -173,18 +168,11 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            title = result["title"]
-            requester = f"[{user_name}](tg://user?id={user_id})"
-            durationmin = result["duration_min"]
-            user = await app.get_users(user_id)
-            photo_id = user.photo.big_file_id if user.photo else None
-            photo = await app.download_media(photo_id)
-            img = await gen_thumb(vidid, photo)
-            button = stream_markup(_, vidid)
-            await ghosttt.reply_photo(
-                photo=img,
-                caption=f"**Track Add To Playlist** {position}\n\n**Song Name** : {title[:30]}\n**Duration Time** :{duration_min}\n**Request By** :{requester}",
-                reply_markup=InlineKeyboardMarkup(button),
+            await app.send_message(
+                original_chat_id,
+                _["queue_4"].format(
+                    position, title[:30], duration_min, user_name
+                ),
             )
         else:
             if not forceplay:
@@ -204,17 +192,15 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            title = result["title"]
-            requester = f"[{user_name}](tg://user?id={user_id})"
-            durationmin = result["duration_min"]
-            user = await app.get_users(user_id)
-            photo_id = user.photo.big_file_id if user.photo else None
-            photo = await app.download_media(photo_id)
-            img = await gen_thumb(vidid, photo)
+            img = await gen_thumb(vidid)
             button = stream_markup(_, vidid)
-            await ghosttt.reply_photo(
+            await app.send_photo(
+                original_chat_id,
                 photo=img,
-                caption=f"**Starting Stream Playing**\n\n**Song Name** : {title[:30]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
+                caption=_["stream_1"].format(
+                    user_name,
+                    f"https://t.me/{app.username}?start=info_{vidid}",
+                ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
 
@@ -287,12 +273,11 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            requester = f"[{user_name}](tg://user?id={user_id})"
-            button = telegram_markup(_)
-            await ghosttt.reply_photo(
-                photo=f"https://telegra.ph/file/0b877fea1da6e376a906b.jpg",
-                caption=f"**File Add To Playlist** » {position}\n\n**Song Name** : {title[:20]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
-                reply_markup=InlineKeyboardMarkup(button),
+            await app.send_message(
+                original_chat_id,
+                _["queue_4"].format(
+                    position, title[:30], duration_min, user_name
+                ),
             )
         else:
             if not forceplay:
@@ -314,11 +299,15 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            requester = f"[{user_name}](tg://user?id={user_id})"
             button = telegram_markup(_)
-            await elnqyb.reply_photo(
-                photo=f"https://telegra.ph/file/0b877fea1da6e376a906b.jpg",
-                caption=f"**Start Streaming Playing**\n\n**Song Name** : {title[:20]}\n**Duration Time** : {duration_min}\n**Request By** :{requester}",
+            await app.send_photo(
+                original_chat_id,
+                photo=config.TELEGRAM_VIDEO_URL
+                if video
+                else config.TELEGRAM_AUDIO_URL,
+                caption=_["stream_4"].format(
+                    title, link, duration_min, user_name
+                ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
     elif streamtype == "live":
@@ -367,11 +356,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            title = result["title"]
-            user = await app.get_users(user_id)
-            photo_id = user.photo.big_file_id if user.photo else None
-            photo = await app.download_media(photo_id)
-            img = await gen_thumb(vidid, photo)
+            img = await gen_thumb(vidid)
             button = telegram_markup(_)
             await app.send_photo(
                 original_chat_id,
