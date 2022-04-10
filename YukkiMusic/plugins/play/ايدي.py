c8f3b19e28ab8891174905c2e63db606ae3b514e
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-10 15:00:42 +0200

    Update ايدي.py

--- YukkiMusic/plugins/play/ايدي.py
+++ YukkiMusic/plugins/play/ايدي.py
@@ -63,7 +63,7 @@ async def khalid(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""**◂ اسمك ↫** {message.from_user.mention}\n\n**◂ معرفك ↫** @{message.from_user.username}\n\n**◂ ايديك ↫** `{message.from_user.Bio}`\n\n**◂ ايدي الجروب ↫** `{message.chat.id}`""", 
+                    await message.reply_photo(photo.file_id,       caption=f"""**◂ اسمك ↫** {message.from_user.mention}\n\n**◂ معرفك ↫** @{message.from_user.username}\n\n**◂ ايديك ↫** `{message.from_user.id}`\n\n**◂ ايدي الجروب ↫** `{message.chat.id}`""", 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
