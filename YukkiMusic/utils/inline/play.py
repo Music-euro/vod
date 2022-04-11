488bce49b34a50daad025c8a41bc4d4dd080bdd8
GG-TT <102429363+GG-TT@users.noreply.github.com>
2022-04-11 07:09:24 +0200

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -11,7 +11,6 @@ from pyrogram.types import Message
 from config import BANNED_USERS
 from strings import get_command
 from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import is_music_playing, music_off
 from YukkiMusic.utils.decorators import AdminRightsCheck
 from strings import get_command
