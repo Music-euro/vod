b49dc18bbf555f637271ef350d0af9698250ce94
Music-euro <108573536+Music-euro@users.noreply.github.com>
2022-07-03 00:05:16 +0200

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -58,7 +58,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GG-TT/allura",
+    "https://github.com/Music-euro/vod.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
